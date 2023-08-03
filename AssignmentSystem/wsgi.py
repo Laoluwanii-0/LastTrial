2063a6ffec4ec3e710f929acc588f7bc62058579
Laoluwanii-0 <olaoluwanioseni@gmail.com>
2023-08-03 07:27:10 +0100

    another

--- AssignmentSystem/wsgi.py
+++ AssignmentSystem/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AssignmentSystem.settings')
 
 application = get_wsgi_application()
+
+app = application
